52dd1ae2d3b67653e0966ad20d797c3a4088af73
Cem Demirayak <cemdemirayak@Cems-MacBook-Air.local>
2023-08-05 13:44:53 +0200

    initial commit

---
+++ App Structure Research/App_Structure_ResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Structure_ResearchApp.swift
+//  App Structure Research
+//
+//  Created by Cem Demirayak on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct App_Structure_ResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
